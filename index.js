102e68353cf8838d6920fe7766de76488f557e50
geanemr <geaneramos1995@gmail.com>
2023-07-16 20:24:48 -0300

    auth: reconhecendo a extensão do arquivo

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// module.exports = () => {
-//   // ...
-// };
 
 const fs = require("fs");
 const path = require("path");
+// import chalk from 'chalk';
 
 let pathInput = process.argv[2];
 
 fs.promises.readFile(pathInput)
 .then(fileBuffer => {
   console.log(fileBuffer.toString());
+  const fileExtension = (pathInput);
+  console.log(fileExtension);
 }).catch(error => {
   console.error(error.message);
  
 });
+
+
