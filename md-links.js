a9bcc2a5267bbcbeb3bed13d4339f6bb71be6d05
geanemr <geaneramos1995@gmail.com>
2023-07-20 11:14:54 -0300

    refactor: passando os comandos para cli.js

--- md-links.js
+++ md-links.js
@@ -81,23 +81,9 @@ function mdLinks(filePath, options = { validate: false }, callback) {
   });
 }
 
-const filePath = "./test.md";
-const validate = process.argv[2] === "--validate";
 
-mdLinks(filePath, { validate: validate }, (error, links) => {
-  if (error) {
-    console.error(error);
-  } else {
-    links.forEach((link) => {
-      console.log(cyan("href:" + link.href));
-      console.log(magenta("text:" + link.text));
-      console.log(yellow("file:" + link.file));
-      if (validate) {
-        console.log(white("status:" + link.status));
-        console.log(white("ok:" + link.ok));
-      }
-      console.log("------------------------");
-    });
-  }
-});
-
+module.exports = {
+  findMdFileURLs,
+  validateMdLink,
+  mdLinks,
+}
