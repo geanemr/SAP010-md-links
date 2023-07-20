5d028898f2f365de5ecb311d2d9beb71ae2d416a
geanemr <geaneramos1995@gmail.com>
2023-07-20 13:24:10 -0300

    refactor: refatoração da função que mostra o validate e o stats

--- cli.js
+++ cli.js
@@ -2,23 +2,9 @@ const { mdLinks } = require("./md-links");
 
 const filePath = "./test.md";
 const validate = process.argv[2] === "--validate";
-const { yellow, magenta, cyan, white, } = require('colorette');
+const stats = process.argv[2] === "--stats";
+const options = {validate, stats};
 
-mdLinks(filePath, { validate: validate }, (error, links) => {
-  if (error) {
-    console.error(error);
-  } else {
-    links.forEach((link) => {
-      console.log(cyan("href:" + link.href));
-      console.log(magenta("text:" + link.text));
-      console.log(yellow("file:" + link.file));
-      if (validate) {
-        console.log(white("status:" + link.status));
-        console.log(white("ok:" + link.ok));
-      }
-      console.log("------------------------");
-    });
-  }
-});
+mdLinks(filePath, options);
 
 
